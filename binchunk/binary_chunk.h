72809e2e955d24fb0e99f60d671d29415ba5bca3
King19931229 <562538332@qq.com>
2020-11-02 22:47:05 +0800

    提交代码

--- binchunk/binary_chunk.h
+++ binchunk/binary_chunk.h
@@ -10,6 +10,54 @@ using UInt64 = unsigned long long int;
 static_assert(sizeof(UInt64) == 8, "size check");
 using String = std::string;
 
+#include <stdarg.h>
+
+struct Format
+{
+	static String FormatString(const char* pszFormat, ...)
+	{
+	#if defined(_MSC_VER)
+	#	define SNPRINTF sprintf_s
+	#	define VSNPRINTF vsnprintf
+	#	pragma warning(disable : 4996)
+	#else
+	#	define SNPRINTF snprintf
+	#	define VSNPRINTF vsnprintf
+	#endif
+		String format;
+
+		va_list list;
+		va_start(list, pszFormat);
+
+		char szBuffer[2048]; szBuffer[0] = '\0';
+
+		int requireBufferSize = VSNPRINTF(szBuffer, sizeof(szBuffer) - 1, pszFormat, list);
+		if (requireBufferSize > sizeof(szBuffer) - 1)
+		{
+			char *szAllocBuffer = new char[requireBufferSize + 1];
+			memset(szAllocBuffer, 0, requireBufferSize + 1);
+			VSNPRINTF(szAllocBuffer, requireBufferSize/* + 1 - 1 */, pszFormat, list);
+			format = szAllocBuffer;
+			delete[] szAllocBuffer;
+		}
+		else
+		{
+			format = szBuffer;
+		}
+
+		va_end(list);
+		return format;
+
+	#undef SNPRINTF
+	#undef VSNPRINTF
+	}
+
+	static inline String FromBool(bool b) { return b ? "true" : "false"; }
+	static inline String FromFloat64(double d) { return std::to_string(d); }
+	static inline String FromUInt64(UInt64 i) { return std::to_string(i); }
+	static inline String FromStirng(const String& s) { return s; };
+};
+
 struct BinaryChunk
 {
 	struct Head
@@ -96,4 +144,93 @@ struct Prototype
 	std::vector<UInt32> LineInfo;
 	std::vector<LocVar> LocVars; 
 	std::vector<std::string> UpvalueNames;
+
+	void PrintHeader() const
+	{
+		String funcType = "main";
+		if(LineDefined > 0)
+			funcType = "function";
+		String varargFlag = "";
+		if(IsVararg)
+			varargFlag += "+";
+		printf("\n$s <%s:%d,%d> (%d instructions)\n", funcType.c_str(), Source,
+			LineDefined, LastLineDefined, Code.size());
+		printf("%d%s parmas, %d slots, %d upvalues, ", NumParams, varargFlag.c_str(),
+			MaxStackSize, UpvalueNames.size());
+		printf("%d locals, %d constants, %d functions\n", LocVars.size(), Constants.size(), 
+		Protos.size());
+	}
+
+	void PrintCode() const
+	{
+		for(size_t pc = 0; pc < Code.size(); ++pc)
+		{
+			UInt32 c = Code[pc];
+			String line = "-";
+			if(LineInfo.size() > 0)
+				line = Format::FormatString("%d", LineInfo[pc]);
+			printf("\t%d\t[%s]\t0x%08X\n", pc + 1, line.c_str(), c);
+		}
+	}
+
+	static String ConstantToString(const Constant& k)
+	{
+		switch (k.tag)
+		{
+			case TAG_NIL:
+				return "";
+			case TAG_BOOLEAN:
+				return Format::FromBool(k.boolean);
+			case TAG_NUMBER:
+				return Format::FromFloat64(k.luaNum);
+			case TAG_INTEGER:
+				return Format::FromUInt64(k.luaInteger);
+			case TAG_SHORT_STR:
+			case TAG_LONG_STR:
+				return k.str;
+			default:
+				return "";
+		}
+	}
+
+	String UpvalName(size_t idx) const
+	{
+		if(UpvalueNames.size() > idx)
+			return UpvalueNames[idx];
+		return "-";
+	}
+
+	void PrintDetail() const
+	{
+		printf("constants (%d):\n", Constants.size());
+		for(size_t i = 0; i < Constants.size(); ++i)
+		{
+			const Constant& k = Constants[i];
+			printf("\t%d\t%s\n", i + 1, ConstantToString(k).c_str());
+		}
+		printf("locals (%d):\n", LocVars.size());
+		for(size_t i = 0; i < LocVars.size(); ++i)
+		{
+			const LocVar& locVar = LocVars[i];
+			printf("\t%d\t%s\t%d\t%d\n", i, locVar.VarName.c_str(),
+				locVar.StartPC + 1, locVar.EndPC + 1);
+		}
+		printf("upvalues (%d):\n", Upvalues.size());
+		for(size_t i = 0; i < Upvalues.size(); ++i)
+		{
+			const Upvalue& upval = Upvalues[i];
+			printf("\t%d\t%s\t\%d\t%d", i, UpvalName(i).c_str(), upval.Instack, upval.Idx);
+		}
+	}
+
+	void List() const
+	{
+		PrintHeader();
+		PrintCode();
+		PrintDetail();
+		for(Prototype* p : Protos)
+		{
+			p->List();
+		}
+	}
 };
--- main.cpp
+++ main.cpp
@@ -23,5 +23,6 @@ int main()
 		f = NULL;
 
 		Prototype* type = Undump(buffer);
+		type->List();
 	}
 }
