84b3bab9d051a1e6b4f47cb1cc8d62ef12ae820a
King19931229 <562538332@qq.com>
2020-12-13 21:40:39 +0800

    第17章

--- codegen.cpp
+++ codegen.cpp
@@ -1,5 +1,4 @@
 #include "compiler/codegen/func_info.h"
-#include "vm/opcodes.h"
 
 int FuncInfo::IndexOfConstant(const LuaValue& k)
 {
@@ -96,6 +95,7 @@ int FuncInfo::AddLocVar(const String& name)
 	locVars.emplace_back(newVar);
 	// use [] to handle duplicated keys in unordered_map.
 	locNames[name] = newVar;
+	return newVar->slot;
 }
 
 int FuncInfo::SlotOfLocVar(const String& name)
@@ -132,9 +132,9 @@ void FuncInfo::ExitScope()
 	{
 		for(int pc : *pendingBreakJmps)
 		{
-			int sBx = /*todo PC()*/ - pc;
+			int sBx = PC() - pc;
 			int i = (sBx + MAXARG_sBX) << 14 | a << 6 | OP_JMP;
-			// todo insts[pc] = (UInt32)i;
+			insts[pc] = (UInt32)i;
 		}
 	}
 }
@@ -207,4 +207,256 @@ int FuncInfo::IndexOfUpval(const String& name)
 		}
 	}
 	return -1;
+}
+
+int FuncInfo::PC()
+{
+	return (int)insts.size() - 1;
+}
+
+void FuncInfo::FixSbx(int pc, int sBx)
+{
+	UInt32 i = insts[pc];
+	// clear sBx
+	i = i << 18 >> 18;
+	// reset Sbx
+	i = i | UInt32(sBx + MAXARG_sBX) << 14;
+	insts[pc] = i;
+}
+
+void FuncInfo::EmitABC(int opcode, int a, int b, int c)
+{
+	UInt32 i = b << 23 | c << 14 | a << 6 | opcode;
+	insts.emplace_back(i);
+}
+
+void FuncInfo::EmitABx(int opcode, int a, int bx)
+{
+	UInt32 i = bx << 14 | a << 6 | opcode;
+	insts.emplace_back(i);
+}
+
+void FuncInfo::EmitAsBx(int opcode, int a, int b)
+{
+	UInt32 i = (b + MAXARG_sBX) << 14 | a << 6 | opcode;
+	insts.emplace_back(i);
+}
+
+void FuncInfo::EmitAx(int opcode, int a)
+{
+	UInt32 i = a << 6 | opcode;
+	insts.emplace_back(i);
+}
+
+// r[a] = r[b]
+void FuncInfo::EmitMove(int a, int b)
+{
+	EmitABC(OP_MOVE, a, b, 0);
+}
+
+// r[a], r[a+1], ..., r[a+b] = nil
+void FuncInfo::EmitLoadNil(int a, int n)
+{
+	EmitABC(OP_LOADNIL, a, n - 1, 0);
+}
+
+// r[a] = (bool)b; if (c) pc++
+void FuncInfo::EmitLoadBool(int a, int b, int c)
+{
+	EmitABC(OP_LOADBOOL, a, b, c);
+}
+
+// r[a] = kst[bx]
+void FuncInfo::EmitLoadK(int a, const LuaValue& k)
+{
+	int idx = IndexOfConstant(k);
+	if(idx < (1 << 18))
+	{
+		EmitABx(OP_LOADKX, a, idx);
+	}
+	else
+	{
+		EmitABx(OP_LOADKX, a, 0);
+		EmitAx(OP_EXTRAARG, idx);
+	}
+}
+
+// r[a], r[a+1], ..., r[a+b-2] = vararg
+void FuncInfo::EmitVararg(int a, int n)
+{
+	// b - 1 = n ---> b = n + 1
+	// b - 1 = n + 1 - 1 = n
+	EmitABC(OP_VARARG, a, n + 1, 0);
+}
+
+// r[a], r[a+1], ..., r[a+b-2] = vararg
+void FuncInfo::EmitClosure(int a, int bx)
+{
+	EmitABx(OP_CLOSURE, a, bx);
+}
+
+// r[a] = {}
+void FuncInfo::EmitNewTable(int a, int nArr, int nRec)
+{
+	EmitABC(OP_NEWTABLE, a, Int2fb(nArr), Int2fb(nRec));
+}
+
+// r[a][(c-1)*FPF+i] := r[a+i], 1 <= i <= b
+void FuncInfo::EmitSetList(int a, int b, int c)
+{
+	EmitABC(OP_SETLIST, a, b, c);
+}
+
+// r[a] := r[b][rk(c)]
+void FuncInfo::EmitGetTable(int a, int b, int c)
+{
+	EmitABC(OP_GETTABLE, a, b, c);
+}
+
+// r[a][rk(b)] = rk(c)
+void FuncInfo::EmitSetTable(int a, int b, int c)
+{
+	EmitABC(OP_SETTABLE, a, b, c);
+}
+
+// r[a] = upval[b]
+void FuncInfo::EmitGetUpval(int a, int b)
+{
+	EmitABC(OP_GETUPVAL, a, b, 0);
+}
+
+// upval[b] = r[a]
+void FuncInfo::EmitSetUpval(int a, int b)
+{
+	EmitABC(OP_SETUPVAL, a, b, 0);
+}
+
+// r[a] = upval[b][rk(c)]
+void FuncInfo::EmitGetTabUp(int a, int b, int c)
+{
+	EmitABC(OP_GETTABUP, a, b, c);
+}
+
+// upval[a][rk(b)] = rk(c)
+void FuncInfo::EmitSetTabUp(int a, int b, int c)
+{
+	EmitABC(OP_SETTABUP, a, b, c);
+}
+
+// r[a], ..., r[a+c-2] = r[a](r[a+1], ..., r[a+b-1])
+void FuncInfo::EmitCall(int a, int nArgs, int nRet)
+{
+	// b - 1 - 1 + 1 = nArgs ---> b = nArgs + 1
+	// c - 1 = nRet --> c = nRet + 1
+	EmitABC(OP_CALL, a, nArgs + 1, nRet + 1);
+}
+
+// return r[a], ... ,r[a+b-2]
+void FuncInfo::EmitTailCall(int a, int nArgs)
+{
+	// b - 2 + 1 = nArgs --> b = nArgs + 1
+	EmitABC(OP_TAILCALL, a, nArgs + 1, 0);
+}
+
+// return r[a], ... ,r[a+b-2]
+void FuncInfo::EmitReturn(int a, int n)
+{
+	// b - 2 + 1 = n --> b = n + 1
+	EmitABC(OP_RETURN, a, n + 1, 0);
+}
+
+// r[a+1] := r[b]; r[a] := r[b][rk(c)]
+void FuncInfo::EmitSelf(int a, int b, int c)
+{
+	EmitABC(OP_SELF, a, b, c);
+}
+
+// pc+=sBx; if (a) close all upvalues >= r[a - 1]
+int FuncInfo::EmitJmp(int a, int sBx)
+{
+	EmitAsBx(OP_JMP, a, sBx);
+	return (int)insts.size() - 1;
+}
+
+// if not (r[a] <=> c) then pc++
+void FuncInfo::EmitTest(int a, int c)
+{
+	EmitABC(OP_TEST, a, 0, c);
+}
+
+// if (r[b] <=> c) then r[a] := r[b] else pc++
+void FuncInfo::EmitTestSet(int a, int b, int c)
+{
+	EmitABC(OP_TESTSET, a, b, c);
+}
+
+int FuncInfo::EmitForPrep(int a, int sBx)
+{
+	EmitAsBx(OP_FORPREP, a, sBx);
+	return (int)insts.size() - 1;
+}
+
+int FuncInfo::EmitForLoop(int a, int sBx)
+{
+	EmitAsBx(OP_FORLOOP, a, sBx);
+	return (int)insts.size() - 1;
+}
+
+void FuncInfo::EmitTForCall(int a, int c)
+{
+	EmitABC(OP_TFORCALL, a, 0, c);
+}
+
+void FuncInfo::EmitTForLoop(int a, int sBx)
+{
+	EmitAsBx(OP_TFORLOOP, a, sBx);
+}
+
+// r[a] = op r[b]
+void FuncInfo::EmitUnaryOp(int op, int a, int b)
+{
+	switch(op)
+	{
+	case TOKEN_OP_NOT:
+		EmitABC(OP_NOT, a, b, 0);
+	case TOKEN_OP_BNOT:
+		EmitABC(OP_BNOT, a, b, 0);
+	case TOKEN_OP_LEN:
+		EmitABC(OP_LEN, a, b, 0);
+	case TOKEN_OP_UNM:
+		EmitABC(OP_UNM, a, b, 0);
+	}
+}
+
+// r[a] = rk[b] op rk[c]
+// arith & bitwise & relational
+void FuncInfo::EmitBinaryOp(int op, int a, int b, int c)
+{
+	auto it = arithAndBitwiseBinops.find(op);
+	if(it != arithAndBitwiseBinops.end())
+	{
+		int opcode = it->second;
+		EmitABC(opcode, a, b, c);
+	}
+	else
+	{
+		switch(op)
+		{
+			case TOKEN_OP_EQ:
+				EmitABC(OP_EQ, 1, b, c);
+			case TOKEN_OP_NE:
+				EmitABC(OP_EQ, 0, b, c);
+			case TOKEN_OP_LT:
+				EmitABC(OP_LT, 1, b, c);
+			case TOKEN_OP_GT:
+				EmitABC(OP_LT, 1, c, b);
+			case TOKEN_OP_LE:
+				EmitABC(OP_LE, 1, b, c);
+			case TOKEN_OP_GE:
+				EmitABC(OP_LE, 1, c, b);
+		}
+		EmitJmp(0, 1);
+		EmitLoadBool(a, 0, 1);
+		EmitLoadBool(a, 1, 0);
+	}
 }
--- compiler/codegen/func_info.h
+++ compiler/codegen/func_info.h
@@ -3,6 +3,7 @@
 #include "compiler/parser/parser.h"
 #include "compiler/lexer/lexer.h"
 #include "state/lua_value.h"
+#include "vm/opcodes.h"
 
 struct LocVarInfo;
 using LocVarInfoPtr = std::shared_ptr<LocVarInfo>;
@@ -65,6 +66,7 @@ struct FuncInfo
 	FuncInfoPtr parent;
 	std::unordered_map<String, UpvalInfoPtr> upvalues;
 	// Bytecode
+	std::unordered_map<int, int> arithAndBitwiseBinops;
 	std::vector<UInt32> insts;
 
 	FuncInfo()
@@ -72,6 +74,19 @@ struct FuncInfo
 		usedRegs = 0;
 		maxRegs = 0;
 		scopeLv = 0;
+
+		arithAndBitwiseBinops.insert({TOKEN_OP_ADD, OP_ADD});
+		arithAndBitwiseBinops.insert({TOKEN_OP_SUB, OP_SUB});
+		arithAndBitwiseBinops.insert({TOKEN_OP_MUL, OP_MUL});
+		arithAndBitwiseBinops.insert({TOKEN_OP_MOD, OP_MOD});
+		arithAndBitwiseBinops.insert({TOKEN_OP_POW, OP_POW});
+		arithAndBitwiseBinops.insert({TOKEN_OP_DIV, OP_DIV});
+		arithAndBitwiseBinops.insert({TOKEN_OP_IDIV, OP_IDIV});
+		arithAndBitwiseBinops.insert({TOKEN_OP_BAND, OP_BAND});
+		arithAndBitwiseBinops.insert({TOKEN_OP_BOR, OP_BOR});
+		arithAndBitwiseBinops.insert({TOKEN_OP_BXOR, OP_BXOR});
+		arithAndBitwiseBinops.insert({TOKEN_OP_SHL, OP_SHL});
+		arithAndBitwiseBinops.insert({TOKEN_OP_SHR, OP_SHR});
 	}
 
 	int IndexOfConstant(const LuaValue& k);
@@ -89,4 +104,38 @@ struct FuncInfo
 	void RemoveLocVar(LocVarInfoPtr locVar);
 
 	int IndexOfUpval(const String& name);
+
+	int PC();
+	void FixSbx(int pc, int sBx);
+	void EmitABC(int opcode, int a, int b, int c);
+	void EmitABx(int opcode, int a, int bx);
+	void EmitAsBx(int opcode, int a, int b);
+	void EmitAx(int opcode, int a);
+	void EmitMove(int a, int b);
+	void EmitLoadNil(int a, int n);
+	void EmitLoadBool(int a, int b, int c);
+	void EmitLoadK(int a, const LuaValue& k);
+	void EmitVararg(int a, int n);
+	void EmitClosure(int a, int bx);
+	void EmitNewTable(int a, int nArr, int nRec);
+	void EmitSetList(int a, int b, int c);
+	void EmitGetTable(int a, int b, int c);
+	void EmitSetTable(int a, int b, int c);
+	void EmitGetUpval(int a, int b);
+	void EmitSetUpval(int a, int b);
+	void EmitGetTabUp(int a, int b, int c);
+	void EmitSetTabUp(int a, int b, int c);
+	void EmitCall(int a, int nArgs, int nRet);
+	void EmitTailCall(int a, int nArgs);
+	void EmitReturn(int a, int n);
+	void EmitSelf(int a, int b, int c);
+	int EmitJmp(int a, int sBx);
+	void EmitTest(int a, int c);
+	void EmitTestSet(int a, int b, int c);
+	int EmitForPrep(int a, int sBx);
+	int EmitForLoop(int a, int sBx);
+	void EmitTForCall(int a, int c);
+	void EmitTForLoop(int a, int sBx);
+	void EmitUnaryOp(int op, int a, int b);
+	void EmitBinaryOp(int op, int a, int b, int c);
 };
